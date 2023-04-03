6812197d3c15af30550fe2ec44fab35f1ace1b7b
Krim-code <suhetsky.gricha555@gmail.com>
2023-04-03 06:40:33 +0300

    Add new model - Dashboard

--- easytodo/user/models.py
+++ easytodo/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Dashboard(models.Model):
+    tasks = models.TextField()
+    user = models.ForeignKey(User, blank=True, null=False,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.tasks} from {self.user}"
