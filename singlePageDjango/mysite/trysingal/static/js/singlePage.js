21d1a169237e9c5c300f8976b239cbc98ecbfbf2
Krim-code <suhetsky.gricha555@gmail.com>
2023-04-10 00:50:46 +0300

    Created script singlePage.js

--- singlePageDjango/mysite/trysingal/static/js/singlePage.js
+++ singlePageDjango/mysite/trysingal/static/js/singlePage.js
@@ -1,6 +1,8 @@
-function changeHeadings(response) {
+function changeHeadings(text,img) {
     let heading = document.querySelector("#change-heading")
-    heading.innerHTML = response
+    heading.innerHTML = text
+    document.body.style.background = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${img} ) no-repeat fixed center`
+    document.body.style.backgroundSize = "cover"
 }
 
 
@@ -10,7 +12,7 @@ addEventListener("DOMContentLoaded",async () => {
         button.onclick = async ()=>{
             let data = await fetch(`api/${button.dataset['page']}`)
             let response = await data.json()
-            changeHeadings(response)
+            await changeHeadings(response.text,response.img)
         }
     })
 })
--- singlePageDjango/mysite/trysingal/views.py
+++ singlePageDjango/mysite/trysingal/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-Json_output = {"1": "First page",
-               "2": "Second page",
-               "3": "Third page"}
+Json_output = {"1": {'text':"First page",
+                     'img':"https://images.unsplash.com/photo-1679081194389-fd2a0f6e1d8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"}
+,
+               "2": {"text":"Second page",
+                     "img":"https://images.unsplash.com/photo-1677226745336-8e04b6643142?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
+,
+               "3": {"text":"Third page",
+                     "img":"https://images.unsplash.com/photo-1679345506039-c4228a79c93a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}}
 
 
 def index(request):
@@ -14,6 +19,6 @@ def index(request):
 
 def api(request, page):
     if str(page) in Json_output.keys():
-        return JsonResponse(Json_output[str(page)],safe=False)
+        return JsonResponse(Json_output[str(page)],safe=True)
     else:
         return JsonResponse("Sorry, i don't have this page :(",safe=False)
