2ff071f082f305dfe20cfee7dd72148bdd86fccc
Krim-code <suhetsky.gricha555@gmail.com>
2023-04-09 21:44:41 +0300

    Created script singlePage.js

--- singlePageDjango/mysite/trysingal/static/js/singlePage.js
+++ singlePageDjango/mysite/trysingal/static/js/singlePage.js
@@ -1,1 +1,16 @@
-alert('hello')
+function changeHeadings(response) {
+    let heading = document.querySelector("#change-heading")
+    heading.innerHTML = response
+}
+
+
+addEventListener("DOMContentLoaded",async () => {
+    let buttons = document.querySelectorAll("button")
+    buttons.forEach(button => {
+        button.onclick = async ()=>{
+            let data = await fetch(`api/${button.dataset['page']}`)
+            let response = await data.json()
+            changeHeadings(response)
+        }
+    })
+})
