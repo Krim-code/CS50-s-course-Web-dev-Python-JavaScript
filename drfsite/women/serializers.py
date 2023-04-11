a552cb63257fca8e1cc72a5d6750e73f9067f4c2
Krim-code <suhetsky.gricha555@gmail.com>
2023-04-11 23:13:35 +0300

    Write SimpleSerializer

--- drfsite/women/serializers.py
+++ drfsite/women/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title','cat_id')
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255),
+    content = serializers.CharField()
+
+
+# def encode():
+#     model = WomenModel('Revi', 'Content: Revi')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
 
